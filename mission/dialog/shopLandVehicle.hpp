19e0e90d48cdbfb542f82ff22210e068837faeda
Florian Kinder <Florian.Kinder@fankserver.com>
2014-04-12 15:03:24 +0200

    WIP updates

---
+++ mission/dialog/shopLandVehicle.hpp
@@ -0,0 +1,72 @@
+class BGD_ShopLandVehicle {
+	idd = 1000;
+	name= "BGD_ShopLandVehicle";
+	movingEnable = false;
+	enableSimulation = true;
+
+	class controlsBackground {
+		class RscTitleBackground: RscText {
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
+			idc = -1;
+			x = 0.1;
+			y = 0.2;
+			w = 0.6;
+			h = (1 / 25);
+		};
+
+		class MainBackground: RscText {
+			colorBackground[] = {0, 0, 0, 0.7};
+			idc = -1;
+			x = 0.1;
+			y = 0.2 + (11 / 250);
+			w = 0.6;
+			h = 0.6 - (22 / 250);
+		};
+	};
+
+	class controls {
+		class Title: RscTitle {
+			colorBackground[] = {0, 0, 0, 0};
+			idc = -1;
+			text = "Land vehicle shop";
+			x = 0.1;
+			y = 0.2;
+			w = 0.6;
+			h = (1 / 25);
+		};
+
+		class ConnectionStatus: Title {
+			idc = 1001;
+			style = 1;
+			text = "";
+		};
+
+		class VehicleList: RscListBox {
+			idc = 1002;
+			text = "";
+			sizeEx = 0.035;
+			x = 0.12; y = 0.26;
+			w = 0.56; h = 0.370;
+		};
+
+		class CloseButtonKey : RscButtonMenu {
+			idc = -1;
+			text = "Close";
+			onButtonClick = "closeDialog 0;";
+			x = -0.06 + (6.25 / 40) + (1 / 250 / (safezoneW / safezoneH));
+			y = 0.8 - (1 / 25);
+			w = (6.25 / 40);
+			h = (1 / 25);
+		};
+
+		class GetCar : RscButtonMenu {
+			idc = -1;
+			text = "Buy";
+			onButtonClick = "[] call life_fnc_unimpound";
+			x = 0.1 + (6.25 / 40) + (1 / 250 / (safezoneW / safezoneH));
+			y = 0.8 - (1 / 25);
+			w = (6.25 / 40);
+			h = (1 / 25);
+		};
+	};
+};
--- mission/functions/Shop/fn_shopVehicles.sqf
+++ mission/functions/Shop/fn_shopVehicles.sqf
@@ -1,1 +1,7 @@
-diag_log "Shop Vehicles";
+createDialog "BGD_ShopLandVehicle";
+
+disableSerialization;
+
+ctrlSetText [1001, "Fetching Vehicles...."];
+
+[[player],"BGDS_fnc_getVehicles"] spawn BIS_fnc_MP;
