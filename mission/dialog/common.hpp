07581aad8455b989d9090f71622af4d20614d116
Florian Kinder <Florian.Kinder@fankserver.com>
2014-04-12 15:28:28 +0200

    More fixes

--- mission/dialog/common.hpp
+++ mission/dialog/common.hpp
@@ -1,3 +1,22 @@
+class BGD_RscScrollBar
+{
+	color[] = {1,1,1,0.6};
+	colorActive[] = {1,1,1,1};
+	colorDisabled[] = {1,1,1,0.3};
+	thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
+	arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+	arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+	border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+	shadow = 0;
+	scrollSpeed = 0.06;
+	width = 0;
+	height = 0;
+	autoScrollEnabled = 1;
+	autoScrollSpeed = -1;
+	autoScrollDelay = 5;
+	autoScrollRewind = 0;
+};
+
 class BGD_RscText {
 	x = 0;
 	y = 0;
@@ -57,7 +76,7 @@ class BGD_RscListBox
 	tooltipColorText[] = {1,1,1,1};
 	tooltipColorBox[] = {1,1,1,1};
 	tooltipColorShade[] = {0,0,0,0.65};
-	class ListScrollBar: Life_RscScrollBar
+	class ListScrollBar: BGD_RscScrollBar
 	{
 		color[] = {1,1,1,1};
 		autoScrollEnabled = 1;
